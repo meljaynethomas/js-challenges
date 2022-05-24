82a0d9312fcac6cd185220e5654a20e3a97d54f0
Mel <meljaynethomas@gmail.com>
2022-05-24 14:19:21 +0100

    js11 challenge complete

--- src/js11-unit-testing/challenge.test.js
+++ src/js11-unit-testing/challenge.test.js
@@ -13,39 +13,32 @@ describe('Should add two numbers together and return the sum', () => {
 });
 
 
-  describe('Compare two numbers and return a string', () => {
-    test('Both numbers are equal', () => {
-      expect(challenge.checkLarger(4, 4)).toEqual("Both numbers are equal");
-    });
-
-    test('First number is larger than second number', () => {
-      let firstNumber = 7;
-      let secondNumber = 2;
-      expect(challenge.checkLarger(firstNumber, secondNumber)).toEqual(`${firstNumber} is bigger than ${secondNumber}`);
-    });
-
-    test('Second number is larger than first number', () => {
-      let firstNumber = 7;
-      let secondNumber = 11;
-      expect(challenge.checkLarger(firstNumber, secondNumber)).toEqual(`${secondNumber} is bigger than ${firstNumber}`);
-    });
-});
-
-
-// export const filterByLength = (names) => {
-//  const validNames = names.filter((name) => name.length <= 6);
-//  if (validNames[0]) {
-//    return validNames;
-//  } else {
-//    return "Sorry, no valid names supplied";
-//  }
-// };
-
-describe('Return an array of valid names', () => {
-  
-  it('Should return valid names', () => {
-    const names = ["dexter", "mel"];
-    expect(challenge.filterByLength(names[0])).toHaveLength(6)
+describe('Compare two numbers and return a string', () => {
+  test('Both numbers are equal', () => {
+    expect(challenge.checkLarger(4, 4)).toEqual("Both numbers are equal");
   });
 
+  test('First number is larger than second number', () => {
+    let firstNumber = 7;
+    let secondNumber = 2;
+    expect(challenge.checkLarger(firstNumber, secondNumber)).toEqual(`${firstNumber} is bigger than ${secondNumber}`);
+  });
+
+  test('Second number is larger than first number', () => {
+    let firstNumber = 7;
+    let secondNumber = 11;
+    expect(challenge.checkLarger(firstNumber, secondNumber)).toEqual(`${secondNumber} is bigger than ${firstNumber}`);
+  });
+});
+
+test("Should return new array of names based on length", () => {
+  expect(challenge.filterByLength(["Paul", "Calum", "Rebecca"])).toMatchObject(["Paul", "Calum"]);
+  expect(challenge.filterByLength(["Johnathan", "Sylvester", "Charlie"])).toBe("Sorry, no valid names supplied");
+  expect(challenge.filterByLength(["Tom", "Dick", "Harry"])).toMatchObject(["Tom", "Dick", "Harry"]);
+});
+
+test("Should reduce all numbers correctly", () => {
+  expect(challenge.reduceNumbers([255, 230, 30], 10)).toMatchObject([245, 220, 20]);
+  expect(challenge.reduceNumbers([40, 30, 20], 50)).toMatchObject([0, 0, 0]);
+  expect(challenge.reduceNumbers([300, 310, 320], 40)).toMatchObject([255, 255, 255]);
 });
